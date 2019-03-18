4c4c3780646ecd44024cde49be48aa13760c5985
Bertrand Giot <bertrand.giot@dotconsulting.be>
2019-03-18 20:34:13 +0100

    Add 16 and 32 configs. Update build script.

---
+++ MK3-BMG-16-copy-config.py
@@ -0,0 +1,4 @@
+import shutil
+
+shutil.copyfile("./Firmware/variants/MK3-BMG-16.h", "./Firmware/Configuration_prusa.h")
+
---
+++ MK3-BMG-32-copy-config.py
@@ -0,0 +1,4 @@
+import shutil
+
+shutil.copyfile("./Firmware/variants/MK3-BMG-32.h", "./Firmware/Configuration_prusa.h")
+
