31ad480f5cbfa217e645015e83783aa9bb5c581f
Giot Bertrand <Bertrand.Giot@sodexo.com>
2019-04-04 15:03:00 +0200

    Automate hex file name generation per variant

---
+++ configure-variant.py
@@ -0,0 +1,21 @@
+import shutil
+from platformio import util
+Import("env")
+
+# print env.Dump()
+
+config = util.load_project_config()
+variantConfig = config.get("env:"+env["PIOENV"] ,"variant_config")
+
+shutil.copyfile("./Firmware/variants/" + variantConfig, "./Firmware/Configuration_prusa.h")
+
+my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
+# print defines
+
+firmwareversion = config.get("platformio" ,"firmware_version")
+variantname = variantConfig.rsplit('.', 1)[0]
+
+filename = "BGIOT-" + variantname + "-" + firmwareversion + "-firmware"
+env.Replace(PROGNAME=filename)
+
