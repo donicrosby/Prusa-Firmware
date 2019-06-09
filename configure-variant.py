1b10a9f20ebe9b15ab72c953610e67a66fe17a01
Bertrand Giot <bertrand.giot@dotconsulting.be>
2019-06-09 22:53:19 +0200

    Update platformio config file

--- configure-variant.py
+++ configure-variant.py
@@ -9,8 +9,8 @@ variantConfig = config.get("env:"+env["PIOENV"] ,"variant_config")
 
 shutil.copyfile("./Firmware/variants/" + variantConfig, "./Firmware/Configuration_prusa.h")
 
-my_flags = env.ParseFlags(env['BUILD_FLAGS'])
-defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
+# my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+# defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
 # print defines
 
 firmwareversion = config.get("platformio" ,"firmware_version")
