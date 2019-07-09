abffd5968e24d16273730414a362d0f1dfb30ba2
Giot Bertrand <Bertrand.Giot@sodexo.com>
2019-07-09 11:29:59 +0200

    Fix build script.improve variants for Z calib.

--- post_build_action.py
+++ post_build_action.py
@@ -1,6 +1,11 @@
+import shutil
 Import("env")
 
 def copy_hex(source, target, env):
-  env.Execute("cp -rf $BUILD_DIR/${PROGNAME}.hex $BUILD_DIR/../${PROGNAME}.hex")
+  buildDir = env['PROJECTBUILD_DIR'] + "/" + env['PIOENV']
+  fileHex = env['PROGNAME'] + ".hex"
+  sourceFile = buildDir + "/" + fileHex
+  destinationFile = buildDir + "/../" + fileHex
+  shutil.copyfile(sourceFile, destinationFile)
 
 env.AddPostAction("buildprog", copy_hex)
