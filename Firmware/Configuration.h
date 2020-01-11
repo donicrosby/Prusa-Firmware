30b79fec0036fa3c33dd0ad238f7ea4f52d7de9e
bgiot <bgiot@dotconsulting.be>
2020-01-11 08:45:24 +0100

    personalize firmware version

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -16,12 +16,12 @@ extern uint16_t nPrinterType;
 extern PGM_P sPrinterName;
 
 // Firmware version
-#define FW_VERSION "3.8.1"
+#define FW_VERSION "3.8.1+"
 #define FW_COMMIT_NR   2869
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
 #define FW_DEV_VERSION FW_VERSION_UNKNOWN
-#define FW_REPOSITORY "Unknown"
+#define FW_REPOSITORY "bgiot"
 #define FW_VERSION_FULL FW_VERSION "-" STR(FW_COMMIT_NR)
 
 // G-code language level
