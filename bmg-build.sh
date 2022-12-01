00d95b4c4b5adf003e42c06c31e4006365a9859f
donicrosby <donicrosby1995@gmail.com>
2022-12-01 08:45:59 -0500

    build: added docker support for build

---
+++ bmg-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+VARIANTS_PATH="${SCRIPT_PATH}/Firmware/variants"
+PRUSA_BUILD_SCRIPT="${SCRIPT_PATH}/PF-build.sh"
+
+for var_file  in $(ls ${VARIANTS_PATH}/*BMG* )
+do
+    if [[ $var_file =~ "MK2" ]]
+    then
+        LANGS="EN_ONLY"
+    else
+        LANGS="ALL"
+    fi
+    variant=$(basename  ${var_file})
+    yes '1' | ${PRUSA_BUILD_SCRIPT} -b Auto -l ${LANGS} -d GOLD -o 1 -c 1 -p 1 -n 1 -v "${variant}"
+done
